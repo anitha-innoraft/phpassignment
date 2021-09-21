18d0b3d99b2a039fb9a9e843e281909fd0806ce2
anitha-innoraft <90893030+anitha-innoraft@users.noreply.github.com>
2021-09-21 16:58:41 +0530

    Add files via upload

--- application/language/english/message_lang.php
+++ application/language/english/message_lang.php
@@ -1,4 +1,29 @@
 <?php
 $lang['all_employees'] = 'All Employees';
 $lang['all_companies'] = 'All Companies';
+$lang['menu_company'] = 'Company';
+$lang['menu_employee'] = 'Employee';
+$lang['companies_list'] = 'Companies list';
+$lang['company_logo'] = "Company Logo";
+$lang['company_name'] = "Company Name";
+$lang['company_email'] = "Company Email";
+$lang['company_website'] = "Company Website";
+$lang['add_company'] = "Add Company";
+$lang['new_company'] = "New Company";
+$lang['update_company'] = "Update Company";
+$lang['Create'] = "Create";
+$lang['Update'] = "Update";
+
+/* Employees*/
+
+$lang['employees_list'] = 'Employees list';
+$lang['employee_name'] = "Employee Name";
+$lang['first_name'] = 'First Name';
+$lang['last_name'] = 'Last Name';
+$lang['employee_email'] = "Employee Email";
+$lang['employee_phone'] = "Employee phone";
+$lang['employee_company'] = "Employee Company";
+$lang['add_employee'] = "Add Employee";
+$lang['new_employee'] = "New employee";
+$lang['update_employee'] = "Update employee";
 ?>
