5c87af87d8520448ab6740052a564984bcc4b4c4
anitha-innoraft <90893030+anitha-innoraft@users.noreply.github.com>
2021-09-21 16:59:22 +0530

    Add files via upload

--- application/language/french/message_lang.php
+++ application/language/french/message_lang.php
@@ -1,4 +1,30 @@
 <?php
 $lang['all_employees'] = 'Tous les employés';
 $lang['all_companies'] = 'Toutes les entreprises';
+$lang['menu_company'] = 'Société';
+$lang['menu_employee'] = 'Employée';
+$lang['companies_list'] = 'Liste des entreprises';
+$lang['company_logo'] = "logo d'entreprise";
+$lang['company_name'] = "Nom de la compagnie";
+$lang['company_email'] = "e-mail de l'entreprise";
+$lang['company_website'] = "Site Web d'entreprise";
+$lang['add_company'] = "Ajouter une entreprise";
+$lang['new_company'] = "Nouvelle compagnie";
+$lang['update_company'] = "société de mise à jour";
+$lang['Create'] = "créer";
+$lang['Update'] = "mettre à jour";
+
+/* Employees*/
+
+$lang['employees_list'] = 'Liste des employés';
+$lang['first_name'] = 'prénom';
+$lang['last_name'] = 'nom de famille';
+$lang['employee_name'] = "Nom de l'employé";
+$lang['employee_email'] = "Courriel de l'employé";
+$lang['employee_phone'] = "Téléphone de l'employé";
+$lang['employee_company'] = "Entreprise salariée";
+$lang['add_employee'] = "Ajouter un employé";
+$lang['new_employee'] = "Nouveau Employé";
+$lang['update_employee'] = "mettre à jour l'employé";
+
 ?>
