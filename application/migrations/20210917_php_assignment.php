256269d1c4d9dbc6a8abd6cb1928de30328a64b7
anitha-innoraft <90893030+anitha-innoraft@users.noreply.github.com>
2021-09-21 16:43:34 +0530

    Add files via upload

--- application/migrations/20210917_php_assignment.php
+++ application/migrations/20210917_php_assignment.php
@@ -1,69 +1,78 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
-class Migration_Initial_Schema extends CI_Migration { 
-    public function up() { 
-            $this->dbforge->add_field(array(
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+class Migration_Initial_Schema extends CI_Migration
+{
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
             'company_id' => array(
-                    'type' => 'INT',
-                    'constraint' => 5,
-                    'unsigned' => TRUE,
-                    'auto_increment' => TRUE
-            ),
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true
+            ) ,
             'company_name' => array(
-                    'type' => 'VARCHAR',
-                    'constraint' => '100'
-            ),
+                'type' => 'VARCHAR',
+                'constraint' => '100'
+            ) ,
             'company_email' => array(
                 'type' => 'VARCHAR',
                 'constraint' => '100'
-            ),
+            ) ,
             'logo' => array(
                 'type' => 'VARCHAR',
-                 'constraint' => '255'
-            ),
-                'Website' => array(
-                    'type' => 'VARCHAR',
-                    'constraint' => '255'
-            ),
+                'constraint' => '255'
+            ) ,
+            'Website' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255'
+            ) ,
         ));
-        $this->dbforge->add_key('company_id', TRUE);
+        $this->dbforge->add_key('company_id', true);
         $this->dbforge->create_table('company');
 
-        $this->dbforge->add_field(array(
+        $this
+            ->dbforge
+            ->add_field(array(
             'employee_id' => array(
-                    'type' => 'INT',
-                    'constraint' => 5,
-                    'unsigned' => TRUE,
-                    'auto_increment' => TRUE
-            ),
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true
+            ) ,
             'first_name' => array(
-                    'type' => 'VARCHAR',
-                    'constraint' => '100'
-            ),
+                'type' => 'VARCHAR',
+                'constraint' => '100'
+            ) ,
             'last_name' => array(
                 'type' => 'VARCHAR',
                 'constraint' => '100'
-            ),
+            ) ,
             'company_id' => array(
                 'type' => 'INT',
-                 'constraint' => '5'
-            ),
-                'email' => array(
-                    'type' => 'VARCHAR',
-                    'constraint' => '255'
-            ),
+                'constraint' => '5'
+            ) ,
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255'
+            ) ,
             'phone' => array(
                 'type' => 'VARCHAR',
                 'constraint' => '10'
-        ),
+            ) ,
         ));
 
-        $this->dbforge->add_key('employee_id', TRUE);
+        $this->dbforge->add_key('employee_id', true);
         $this->dbforge->create_table('employee');
+
+        $this->db->query(add_foreign_key('employee', 'company_id', 'company(company_id)', 'CASCADE', 'CASCADE'));
     }
 
     public function down()
     {
         $this->dbforge->drop_table('company');
         $this->dbforge->drop_table('employee');
+        $this->db->query(drop_foreign_key('employee', 'company_id'));
     }
 }
+
